bf28cddec582b620e01c72e242193d6ca019ea5f
AidenFemby <fembyaiden@gmail.com>
2023-07-28 00:19:39 +0100

    day1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import test from './test.jpg';
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      this is a test commit
+      <h1>RabbitHole</h1>
+        <div className="home-container"></div>
+          <div className="side-bar"></div>
+          <div className="side-bar2"></div>
+            <div className="icon-container"></div>
+            <img
+            srs={test}
+            alt="yes"
+            className="task.icon"
+            onClick={()=> console.log("clicked")}
+            />
     </div>
-   
   );
 }
 
