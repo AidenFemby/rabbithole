499b4b9ee18d9602a9c1b33ce205213cf30d72ec
AidenFemby <fembyaiden@gmail.com>
2023-07-30 15:48:30 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import test from './test.jpg';
+import yes from "./yes.png";
 import "./App.css";
+import { IoIosStats } from "react-icons/io";
+import { BsSpotify } from "react-icons/bs";
+import { CgNotes } from "react-icons/cg";
+import { RiTimerLine } from "react-icons/ri";
+import { SlCalender } from "react-icons/sl";
+import { LuSettings2 } from "react-icons/lu";
+import { AiFillFilePdf } from "react-icons/ai";
 function App() {
   return (
     <div className="App">
       <h1>RabbitHole</h1>
-        <div className="home-container"></div>
-          <div className="side-bar"></div>
-          <div className="side-bar2"></div>
-            <div className="icon-container"></div>
-            <img
-            srs={test}
-            alt="yes"
-            className="task.icon"
-            onClick={()=> console.log("clicked")}
-            />
+      <div className="home-container">
+        <div className="side-bar">
+          <div
+            className="icon-container"
+            style={{
+              borderTopRightRadius: "12px",
+              borderTopLeftRadius: "12px",
+            }}
+          >
+            <BsSpotify className="icon" />
+          </div>
+          <div className="icon-container">
+            <CgNotes className="icon" />
+          </div>
+          <div className="icon-container">
+            <IoIosStats className="icon" />
+          </div>
+          <div className="icon-container">
+            <RiTimerLine className="icon" />
+          </div>
+          <div className="icon-container">
+            <SlCalender className="icon" />
+          </div>
+          <div className="icon-container">
+            <AiFillFilePdf className="icon" />
+          </div>
+          <div className="icon-container radiusbuttom">
+            <LuSettings2 className="icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
